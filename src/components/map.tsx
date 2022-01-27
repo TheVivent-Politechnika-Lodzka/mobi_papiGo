a2b2d9210a8c14ebdfbc372e8b0917915e24c447
Adam Kapuściński <ziemniaczek320@gmail.com>
2022-01-27 08:34:11 +0100

    dodanie przycisków do debugowania

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef } from 'react';
 import MapView, { EventUserLocation, PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet, Alert } from 'react-native';
+import { StyleSheet, Pressable, Text } from 'react-native';
 import {
   generateRandomAnimalMarker,
   AnimalMarkerProps,
   AnimalMarker,
+  ItemMarkerProps,
+  generateRandomItemMarker,
+  ItemMarker,
 } from './marker';
-import { Animal } from '../types';
-import { useAnimals } from '../auth/useFirestore';
+import { Animal, Item } from '../types';
+import { useAnimals, useItems } from '../auth/useFirestore';
 import {
   getRandomFloatInclusive,
   getRandomIntInclusive,
@@ -24,23 +27,17 @@ interface Location {
 }
 
 export default function MapComponent() {
-  const [animals, animalsUpdater] = useAnimals();
   const mapRef = useRef<MapView>(null);
   const [location, setLocation] = React.useState<Location>({} as Location);
+
+  const [animals, animalsUpdater] = useAnimals();
   const [animalMarkers, setAnimalMarkers] = React.useState<AnimalMarkerProps[]>(
     []
   );
+  const [items, itemsUpdater] = useItems();
+  const [itemMarkers, setItemMarkers] = React.useState<ItemMarkerProps[]>([]);
 
-  useEffect(() => {
-    if (location?.altitude === null || location?.altitude === undefined) {
-      return;
-    }
-    // 10% szansy na dodanie nowego markera
-    // pewnie będzie trzeba zmiejszyć
-    if (getRandomFloatInclusive(1, 100) > 10) {
-      return;
-    }
-
+  const generateAnimalMarker = () => {
     const animalMarker = generateRandomAnimalMarker({
       longitude: location.longitude,
       latitude: location.latitude,
@@ -48,7 +45,7 @@ export default function MapComponent() {
     });
     const animalMarkersCopy = [...animalMarkers, animalMarker];
     // ograniczenie na maksymalną ilość markerów
-    if (animalMarkersCopy.length > 10) {
+    if (animalMarkersCopy.length > 3) {
       // usuń losowy marker
       const randomIndex = getRandomIntInclusive(
         0,
@@ -58,6 +55,36 @@ export default function MapComponent() {
     }
 
     setAnimalMarkers(animalMarkersCopy);
+  };
+
+  const generateItemMarker = () => {
+    const itemMarker = generateRandomItemMarker({
+      longitude: location.longitude,
+      latitude: location.latitude,
+      range: 0.0009,
+    });
+    const itemMarkersCopy = [...itemMarkers, itemMarker];
+    // ograniczenie na maksymalną ilość markerów
+    if (itemMarkersCopy.length > 5) {
+      // usuń losowy marker
+      const randomIndex = getRandomIntInclusive(0, itemMarkersCopy.length - 1);
+      itemMarkersCopy.splice(randomIndex, 1);
+    }
+    setItemMarkers(itemMarkersCopy);
+  };
+
+  useEffect(() => {
+    if (location?.altitude === null || location?.altitude === undefined) {
+      return;
+    }
+    // 10% szansy na dodanie nowego markera
+    // pewnie będzie trzeba zmiejszyć
+    if (getRandomFloatInclusive(1, 100) < 5) {
+      generateAnimalMarker();
+    }
+    if (getRandomFloatInclusive(1, 100) < 10) {
+      generateItemMarker();
+    }
   }, [location.longitude, location.latitude]);
 
   useEffect(() => {
@@ -95,36 +122,75 @@ export default function MapComponent() {
     animalsUpdater.addAnimal(animal);
   };
 
+  const handleItemPress = (longitude: number, latitude: number, item: Item) => {
+    const newItemMarkers = itemMarkers.filter(
+      (marker) => marker.item.id !== item.id
+    );
+    setItemMarkers(newItemMarkers);
+    itemsUpdater.addItem(item);
+  };
+
   return (
-    <MapView
-      ref={mapRef}
-      style={styles.map}
-      onUserLocationChange={updateLocation}
-      //  consts:
-      showsUserLocation={true}
-      userLocationPriority="high"
-      userLocationUpdateInterval={500}
-      userLocationFastestInterval={500}
-      provider={PROVIDER_GOOGLE}
-      showsCompass={false}
-      showsScale={false}
-      showsPointsOfInterest={false}
-      showsIndoors={false}
-      showsMyLocationButton={false}
-      toolbarEnabled={false}
-    >
-      {animalMarkers.map((marker: AnimalMarkerProps) => (
-        <AnimalMarker
-          key={marker.animal.id}
-          {...marker}
-          onPress={handleAnimalPress}
-        />
-      ))}
-    </MapView>
+    <>
+      <Pressable style={styles.debugButton} onPress={generateAnimalMarker}>
+        <Text>generuj zwierzaka</Text>
+      </Pressable>
+      <Pressable style={styles.debugButton2} onPress={generateItemMarker}>
+        <Text>generuj obrożę</Text>
+      </Pressable>
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        onUserLocationChange={updateLocation}
+        //  consts:
+        showsUserLocation={true}
+        userLocationPriority="high"
+        userLocationUpdateInterval={500}
+        userLocationFastestInterval={500}
+        provider={PROVIDER_GOOGLE}
+        showsCompass={false}
+        showsScale={false}
+        showsPointsOfInterest={false}
+        showsIndoors={false}
+        showsMyLocationButton={false}
+        toolbarEnabled={false}
+      >
+        {animalMarkers.map((marker: AnimalMarkerProps) => (
+          <AnimalMarker
+            key={marker.animal.id}
+            {...marker}
+            onPress={handleAnimalPress}
+          />
+        ))}
+        {itemMarkers.map((marker: ItemMarkerProps) => (
+          <ItemMarker
+            key={marker.item.id}
+            {...marker}
+            onPress={handleItemPress}
+          />
+        ))}
+      </MapView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  debugButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: 'red',
+    padding: 10,
+    zIndex: 10000,
+  },
+  debugButton2: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: 'red',
+    padding: 10,
+    zIndex: 10000,
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
   },

1fdbd25fbc2590f8f94f2b226f7792b7a240e39f
Adam Kapuściński <ziemniaczek320@gmail.com>
2022-01-27 09:55:02 +0100

    możliwa zmiana nicku

--- src/screens/main/userScreen.tsx
+++ src/screens/main/userScreen.tsx
@@ -1,24 +1,27 @@
 import React, { useEffect } from 'react';
 import useCurrentUser from '../../auth/useCurrentUser';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { logout } from '../../auth/methods';
-import { useItems } from '../../auth/useFirestore';
-import { Item } from '../../types';
 
 import NavBar from '../../components/navBar';
+import { Avatar, Input, Button } from 'react-native-elements';
+import { set } from 'react-native-reanimated';
 
 export default function UserScreen({ navigation }: any) {
-  const [items, itemsUpdater] = useItems();
   const user = useCurrentUser();
   const [photoURL, setPhotoURL] = React.useState(
     'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
   );
+  const [displayName, setDisplayName] = React.useState('');
 
   useEffect(() => {
     if (user?.photoURL) {
       setPhotoURL(user.photoURL);
     }
-  }, [user?.photoURL]);
+    if (user?.displayName) {
+      setDisplayName(user.displayName);
+    }
+  }, [user?.photoURL, user?.displayName]);
 
   if (user === null) {
     return (
@@ -30,12 +33,20 @@ export default function UserScreen({ navigation }: any) {
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: photoURL }} style={styles.image} />
+      <Avatar size={256} rounded source={{ uri: photoURL }} />
       <Text>{user.email}</Text>
-      <Text>{user.displayName}</Text>
+      <View style={styles.displayNameContainer}>
+        <Input
+          containerStyle={{ width: 300 }}
+          defaultValue={displayName}
+          onChangeText={(newDisplayName) => setDisplayName(newDisplayName)}
+        />
+        <Button
+          title="Zapisz"
+          onPress={() => user.updateProfile({ displayName })}
+        />
+      </View>
       <Button title="Wyloguj" onPress={logout} />
-      <Text>{JSON.stringify(items)}</Text>
-      <Button title="test" onPress={() => itemsUpdater.addItem(new Item())} />
       <NavBar navigation={navigation} />
     </View>
   );
@@ -46,8 +57,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#87AAAA',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     ...StyleSheet.absoluteFillObject,
+  },
+  displayNameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   image: {
     borderRadius: 100,
